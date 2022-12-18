63577576192f26e217bc67c3385af5b21f9e01b2
Dong Cat <legorussiablock@gmail.com>
2022-12-19 02:18:00 +0700

    Finish 1.7

--- L7.py
+++ L7.py
@@ -2,6 +2,7 @@
 # b) Do Chinese remaindering for 12345 → 6789, 56789 → 54292.
 # c) Do chinese remaindering for 41 → 30, 152 → 87, 263 → 127. 
 
+import math
 #%%
 def extended_euclidean(a, b):
     if a == 0:
@@ -42,5 +43,31 @@ x = [5**4, 0]
 print("a) Last 4 digits of A:", crt(m, x)%10000)
 
 #%%
-# 12345 mod x = 6789
-# 56789 mod x = 54292
+m = [12345, 56789]
+x = [6789, 54292]
+tmp = m.copy()
+a = 0
+n = math.prod(m)
+for i in range(2):
+    tmp[i] = n // m[i]
+    b = tmp[i] % m[i]
+    t = modinv(b, m[i])
+    e = tmp[i] * t
+    a += x[i] * e
+a = a % n
+print("b) A =", a)
+
+#%%
+m = [41, 152, 263]
+x = [30, 87, 127]
+tmp = m.copy()
+a = 0
+n = math.prod(m)
+for i in range(2):
+    tmp[i] = n // m[i]
+    b = tmp[i] % m[i]
+    t = modinv(b, m[i])
+    e = tmp[i] * t
+    a += x[i] * e
+a = a % n
+print("b) C =", a)
